af5c8beea04b4571ffaefd368b68c0397a421e48
Jana Alexis Paner <alexis.jana10@gmail.com>
2017-09-26 17:18:00 +0800

    added a script for the effect on the navigation

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".fa-times").click(function(){
+		$(".menu").addClass("hide_menu");
+		$(".toggle_menu").addClass("opacity_one");
+	});
+
+	$(".toggle_menu").click(function(){
+		$(".menu").removeClass("hide_menu");
+		$(".toggle_menu").removeClass("opacity_one");
+	});
+
+});
